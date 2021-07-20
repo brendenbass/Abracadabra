5b2bde7df2563e22eb8c5b9ad9a694fd53478617
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-07-20 12:29:43 -0400

    Structured app.html page; started on js

--- JS/site.js
+++ JS/site.js
@@ -1,4 +1,10 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hello World");
-}
+//get values from user input
+//starts or controller function
+function getValues()
+
+
+//generate numbers from the start value to the end value
+//logic function(s)
+
+
+//display numbers and make start and 
