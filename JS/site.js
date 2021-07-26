b7d3cab1bf6da5b5b9338f956e20ab615deac9d6
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-07-26 13:20:35 -0400

    Fixed button link

--- JS/site.js
+++ JS/site.js
@@ -19,6 +19,8 @@ function getValues(){
     }
 }
 
+
+
 function abraCadabra(abraValue,cadabraValue) {
 
     let returnArray = [];
@@ -45,6 +47,8 @@ function abraCadabra(abraValue,cadabraValue) {
     }
     return returnArray;
 }
+
+
 
 //loop over the array and create a tablerow for each item
 function displayData(acArray) {
