8dd6dc23197bc7054b74682d80c7db69703db927
Brenden Bass <80184025+brendenbass@users.noreply.github.com>
2021-07-26 12:58:20 -0400

    Got the javascript in the app working

--- JS/site.js
+++ JS/site.js
@@ -1,17 +1,17 @@
 //get values from user input
 //starts or controller function
 function getValues(){
-    let startValue = document.getElementById("startValue").value;
-    let endValue = document.getElementById("endValue").value;
+    let abraValue = document.getElementById("startValue").value;
+    let cadabraValue = document.getElementById("endValue").value;
 
     //validate input to make sure that input is numbers
     //parse input for integers
-    startValue = parseInt(startValue);
-    endValue = parseInt(endValue);
+    abraValue = parseInt(abraValue);
+    cadabraValue = parseInt(cadabraValue);
 
-    if(Number.isInteger(startValue) && Number.isInteger(endValue)) {
+    if(Number.isInteger(abraValue) && Number.isInteger(cadabraValue)) {
         //call abracadabra
-        let acArray = Abracadabra(abraValue,cadabraValue);
+        let acArray = abraCadabra(abraValue,cadabraValue);
         //call displayData and write the values to the screen
         displayData(acArray);
     } else {
@@ -24,11 +24,11 @@ function abraCadabra(abraValue,cadabraValue) {
     let returnArray = [];
 
     //loop from 1 to 100
-    for (let index = 0; index <= 100; index++) {
+    for (let index = 1; index <= 100; index++) {
         //need to check current integer in three steps
         //(1) Check to see if divisible by both (3 and 5); if so, push Abracadabra.
         if (index % abraValue == 0 && index % cadabraValue == 0) {
-            returnArray.push("Abracadabra!");
+            returnArray.push("Abracadabra");
             }
         //(2) Check to see if divisible by abra value (3); if so, push Abra.
         else if (index % abraValue == 0) {
@@ -43,7 +43,7 @@ function abraCadabra(abraValue,cadabraValue) {
             returnArray.push(index);
             }
     }
-    returnArray;
+    return returnArray;
 }
 
 //loop over the array and create a tablerow for each item
@@ -53,17 +53,32 @@ function displayData(acArray) {
     let tableBody = document.getElementById("results");
 
     //get the template itself
-    let tableRow = document.getElementById("fbTemplate");
+    let templateRow = document.getElementById("fbTemplate");
 
     //clear the table
     tableBody.innerHTML = "";
 
-    for (let index = 0; index < acArray.length; index++) {
+    for (let index = 0; index < acArray.length; index+= 5) {
         let tableRow = document.importNode(templateRow.content, true);
         
         //grab the td to put into array
         let rowCols = tableRow.querySelectorAll("td");
-        rowCols[0].textContent = acData[i];
+        rowCols[0].classList.add(acArray[index]);
+        rowCols[0].textContent = acArray[index];
+
+        rowCols[1].classList.add(acArray[index+1]);
+        rowCols[1].textContent = acArray[index+1];
+
+        rowCols[2].classList.add(acArray[index+2]);
+        rowCols[2].textContent = acArray[index+2];
+
+        rowCols[3].classList.add(acArray[index+3]);
+        rowCols[3].textContent = acArray[index+3];
+
+        rowCols[4].classList.add(acArray[index+4]);
+        rowCols[4].textContent = acArray[index+4];
+
+        tableBody.appendChild(tableRow);
     }
 
     //add all the rows to the table
